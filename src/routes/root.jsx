0e67b349dfabbd5984741d10671cf554726750cc
Jeffrey Li <jli588@myseneca.ca>
2024-09-02 17:24:52 -0400

    fixed navbar import

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 
-import NavBar from "../components/navbar";
+import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { ShoppingCartContext } from "../components/ShoppingCartContext";
 
