b414062796ffdc2ac8cc53987fbc92e92c214b5c
Jeffrey Li <jli588@myseneca.ca>
2024-09-10 17:51:28 -0400

    added testing library userEvent; fixed shoppingCart conditioanl checker when rendering shopping items added by user

--- src/routes/shopping-cart.jsx
+++ src/routes/shopping-cart.jsx
@@ -72,7 +72,7 @@ export default function ShoppingCart() {
         /*         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }} */
       >
-        {shoppingCart.length > 0 ? (
+        {shoppingCart && shoppingCart.length > 0 ? (
           shoppingCart.map((product) => (
             <Grid key={product.title} size={{ xs: 12, sm: 12, md: 12 }}>
               <ShoppingCartCard
@@ -99,7 +99,7 @@ export default function ShoppingCart() {
             sx={{ marginRight: "3px" }}
           >
             <b>Total: </b>
-            {shoppingCart.length > 0
+            {shoppingCart /* .length > 0 */
               ? "$" +
                 shoppingCart
                   .reduce(
